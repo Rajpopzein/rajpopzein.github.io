d1c35c1dcee7e8a59a52bc61a327faed8bc21f19
Rajkumar <46551565+Rajpopzein@users.noreply.github.com>
2019-03-17 19:58:04 +0530

    portfolio

--- css/js/main.js
+++ css/js/main.js
@@ -1,8 +1,33 @@
 $(document).ready(function(){
-    //toggler
-    var toggler = $(".toggler");
-
-    toggler.click(function(){
-        $('.navbar-menu').toggleClass('active');
+    var typed = new Typed('#typed',{
+        strings: ["Hello There","I am Rajkumar","A UI / Web Developer","A Photographer"],
+        typeSpeed:40,
+        backSpeed:40,
+        backDelay:1000,
+        loop:true
     });
+
+    $('.menu').click(function(){
+        $('.navbar nav ul li').toggleClass('active');
+    });
+
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()){
+            $('.nbr').addClass('black');
+        }else{
+            $('.nbr').removeClass('black');
+        }
+    })
+    //scroll 
+    var scrollClick = $('.nav-item');
+    scrollClick.click(function(e){
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop:
+            $(this.hash).offset().top
+        },1000);
+    });
+    //link switch
+    //hash is resprsending id
+    
 });
